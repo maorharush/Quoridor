618da76b501a901ed227927f32b3d82c94a735b6
Mor Merhav <mormerhav@gmail.com>
2018-08-08 15:31:16 +0300

    Rename param

--- quoridorcore/src/main/java/com/harush/zitoon/quoridor/core/logic/Pawn.java
+++ quoridorcore/src/main/java/com/harush/zitoon/quoridor/core/logic/Pawn.java
@@ -4,7 +4,7 @@ import com.harush.zitoon.quoridor.core.model.LogicResult;
 
 public interface Pawn extends BoardPiece {
 
-    LogicResult spawn(int x, int y, Board boardLogic);
+    LogicResult spawn(int x, int y, Board board);
 
-    LogicResult move(int x, int y, Board boardLogic);
+    LogicResult move(int x, int y, Board board);
 }
