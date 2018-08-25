39b7f78482fc8e0c612f2799472ecea6909430c4
Mor Merhav <mormerhav@gmail.com>
2018-08-25 22:19:37 +0300

    Make DumbAIPlayer EVEN DUMBER AGAIN !

--- quoridorcore/src/main/java/com/harush/zitoon/quoridor/core/model/DumbAIPlayer.java
+++ quoridorcore/src/main/java/com/harush/zitoon/quoridor/core/model/DumbAIPlayer.java
@@ -3,26 +3,18 @@ package com.harush.zitoon.quoridor.core.model;
 import java.util.Random;
 
 /**
- * 
  * A very simple AI player.
- *
  */
 public class DumbAIPlayer extends Player {
 
-	public DumbAIPlayer(String name, Pawn pawn, String pawnColour) {
-		super(name, pawn, pawnColour);
-	}
+    public DumbAIPlayer(String name, Pawn pawn, String pawnColour) {
+        super(name, pawn, pawnColour);
+    }
 
-	@Override
-	public void play() {
-		System.out.println("RANDOMLY SELECTING MOVE!!!! I AM DUMB AHHHHH");
-
-		LogicResult logicResult;
-		do {
-
-			logicResult = pawn.move(4, pawn.getY()+1);
-		} while (logicResult == null || !logicResult.isSuccess());
-
-	}
+    @Override
+    public void play() {
+        System.out.println("RANDOMLY SELECTING MOVE!!!! I AM DUMB AHHHHH");
+        pawn.move(4, pawn.getY() + 1);
+    }
 
 }
